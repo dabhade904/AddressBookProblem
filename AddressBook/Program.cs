5c7cae21dc8b1adc3c37d6fdb5da17865d3065de
dabhade904 <dabhade904@gmail.com>
2022-08-04 21:08:45 +0530

    Added : initialise project and added Welcome message

---
+++ AddressBook/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBook
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Address book problem");
+        }
+    }
+}
