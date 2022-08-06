42bc51af252c69b9297e40c051b86382ba33a0b8
dabhade904 <dabhade904@gmail.com>
2022-08-06 14:05:13 +0530

    Added : fuctionality to create a address book

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace AddressBook
+﻿namespace AddressBookProblem
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Address book problem");
+            AddressBook obj = new AddressBook();
+            obj.FirstName = "Sachin";
+            obj.LastName = "Dabhade";
+            obj.Address = "shekta ";
+            obj.City = "Bidkeen";
+            obj.State = "Maharashtra";
+            obj.Zip = 431105;
+            obj.PhoneNumber = 8888948943;
+            obj.Email = "dabhade904@gmail.com";
+            Console.WriteLine("Name :"+obj.FirstName+ " "+obj.LastName+" Address "+obj.Address+" "+obj.City+" "+obj.State+" Pin code "+obj.Zip+" Contact "+obj.PhoneNumber+" "+obj.Email);
         }
     }
 }
+
