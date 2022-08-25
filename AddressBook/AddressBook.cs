8db94119edb1b27cb6b4e3c13bf87e2f35e8001a
dabhade904 <dabhade904@gmail.com>
2022-08-25 18:41:06 +0530

    Refactored : code to ensure to no duplicate person data in address book

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,16 +9,21 @@ namespace AddressBook
 {
     public class AddressBook
     {
-        Dictionary<string, Person> addresses = new Dictionary<string, Person>();
-        public Dictionary<string, Person> createuser()
+        Dictionary<string, List<Person>> addresses = new Dictionary<string, List<Person>>();
+        public Dictionary<string, List<Person>> Createuser()
         {
+            List<Person> personList = new List<Person>();
             string? next = string.Empty;
             bool anotheruser = true;
             try
             {
+                Console.WriteLine("Enter the country you want to add address");
+                var country = Console.ReadLine();
+
                 while (anotheruser)
                 {
                     var person = new Person();
+
                     Console.WriteLine("Enter book name");
                     person.BookName = Console.ReadLine();
                     Console.WriteLine("Enter first name");
@@ -35,8 +41,15 @@ namespace AddressBook
                     Console.WriteLine("Enter  Zip Code");
                     person.Zip = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("Enter Phone Number ");
-                    person.PhoneNumber =Console.ReadLine();
-                    addresses.Add(person.FirstName, person);
+                    person.PhoneNumber = Console.ReadLine();
+                    if (personList.Count == 0)
+                    {
+                        personList.Add(person);
+                    }
+                    else
+                    {
+                        personList = AddDistinctPersonInList(personList, person);
+                    }
                     do
                     {
                         Console.WriteLine("Do you want to add again? press yes/no");
@@ -44,6 +57,7 @@ namespace AddressBook
                     } while (next != "yes" && next != "no" && next != "Yes" && next != "No");
                     anotheruser = (next == "Yes" || next == "yes");
                 }
+                addresses.Add(country, personList);
             }
             catch (Exception)
             {
@@ -51,25 +65,36 @@ namespace AddressBook
             }
             return addresses;
         }
-
-
+        public List<Person> AddDistinctPersonInList(List<Person> list, Person person)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].FirstName != person.FirstName)
+                {
+                    list.Add(person);
+                }
+            }
+            var listOfPerson = list.GroupBy(x => x.FirstName).Select(x => x.First()).ToList();
+            return listOfPerson;
+        }
         public void Display()
         {
             if (addresses.Count != 0)
             {
-                foreach (KeyValuePair<string, Person> dict in addresses)
+                foreach (KeyValuePair<string, List<Person>> kvp in addresses)
                 {
-                    Console.WriteLine("Address Book Name " + dict.Key);
-                    Person person = dict.Value;
-                    Console.WriteLine("First Name : {0} , Last Name : {1} , Address : {2} , City : {3} , State : {4} , Zip : {5} , Phone Number : {6} ",
+                    Console.WriteLine("Key = {0}", kvp.Key);
+                    foreach (var person in kvp.Value)
+                    {
+                        Console.WriteLine("First Name : {0} , Last Name : {1} , Address : {2} , City : {3} , State : {4} , Zip : {5} , Phone Number : {6} ",
                         person.FirstName, person.LastName, person.Address, person.City, person.State, person.Zip, person.PhoneNumber);
+                    }
                 }
             }
             else
             {
                 Console.WriteLine("Dictionary is empty");
             }
-
         }
         public void EditPersonInfo()
         {
@@ -79,42 +104,45 @@ namespace AddressBook
                 string? fName = Console.ReadLine();
                 if (fName != null)
                 {
-                    foreach (KeyValuePair<string, Person> obj in addresses)
+                    foreach (KeyValuePair<string, List<Person>> kvp in addresses)
                     {
-                        if (fName.Equals(obj.Value.FirstName))
+                        foreach (var person in kvp.Value)
                         {
-                            Console.WriteLine("choose field you want to edit");
-                            int input = Convert.ToInt32(Console.ReadLine());
-                            switch (input)
+                            if (fName.Equals(person.FirstName))
                             {
-                                case 1:
-                                    Console.WriteLine("Re-corect your First name");
-                                    obj.Value.FirstName = Console.ReadLine();
-                                    break;
-                                case 2:
-                                    Console.WriteLine("Re-corect your last name");
-                                    obj.Value.LastName = Console.ReadLine();
-                                    break;
-                                case 3:
-                                    Console.WriteLine("Re-corect your address");
-                                    obj.Value.Address = Console.ReadLine();
-                                    break;
-                                case 4:
-                                    Console.WriteLine("Re-corect your City Name");
-                                    obj.Value.City = Console.ReadLine();
-                                    break;
-                                case 5:
-                                    Console.WriteLine("Re-corect your State Name");
-                                    obj.Value.State = Console.ReadLine();
-                                    break;
-                                case 6:
-                                    Console.WriteLine("Re-corect your Zip Code");
-                                    obj.Value.Zip = Convert.ToInt32(Console.ReadLine());
-                                    break;
-                                case 7:
-                                    Console.WriteLine("Re-corect your Phone Number");
-                                    obj.Value.PhoneNumber =Console.ReadLine();
-                                    break;
+                                Console.WriteLine("choose field you want to edit");
+                                int input = Convert.ToInt32(Console.ReadLine());
+                                switch (input)
+                                {
+                                    case 1:
+                                        Console.WriteLine("Re-corect your First name");
+                                        person.FirstName = Console.ReadLine();
+                                        break;
+                                    case 2:
+                                        Console.WriteLine("Re-corect your last name");
+                                        person.LastName = Console.ReadLine();
+                                        break;
+                                    case 3:
+                                        Console.WriteLine("Re-corect your address");
+                                        person.Address = Console.ReadLine();
+                                        break;
+                                    case 4:
+                                        Console.WriteLine("Re-corect your City Name");
+                                        person.City = Console.ReadLine();
+                                        break;
+                                    case 5:
+                                        Console.WriteLine("Re-corect your State Name");
+                                        person.State = Console.ReadLine();
+                                        break;
+                                    case 6:
+                                        Console.WriteLine("Re-corect your Zip Code");
+                                        person.Zip = Convert.ToInt32(Console.ReadLine());
+                                        break;
+                                    case 7:
+                                        Console.WriteLine("Re-corect your Phone Number");
+                                        person.PhoneNumber = Console.ReadLine();
+                                        break;
+                                }
                             }
                         }
                     }
@@ -139,16 +167,19 @@ namespace AddressBook
                     string? fName = Console.ReadLine();
                     if (fName != null)
                     {
-                        foreach (KeyValuePair<string, Person> obj in addresses)
+                        foreach (KeyValuePair<string, List<Person>> kvp in addresses)
                         {
-                            if (fName.Equals(obj.Key))
+                            foreach (var person in kvp.Value)
                             {
-                                addresses.Remove(obj.Key);
-                                Console.WriteLine("data removed successfully");
-                            }
-                            else
-                            {
-                                Console.WriteLine("Data is not available");
+                                if (fName.Equals(person.FirstName))
+                                {
+                                    addresses.Remove(kvp.Key);
+                                    Console.WriteLine("data removed successfully");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Data is not available");
+                                }
                             }
                         }
                     }
@@ -165,27 +196,6 @@ namespace AddressBook
             catch (Exception)
             {
                 Console.WriteLine("Enter valid input");
-            }
-        }
-
-        public void SearchPerson()
-        {
-            Console.WriteLine("Enter person you want to search");
-            string? person = Console.ReadLine();
-            if (person != null)
-            {
-                var search = addresses.Where(p => p.Value.FirstName.Contains(person));
-                foreach (KeyValuePair<string, Person> dict in search)
-                {
-                    Console.WriteLine("Address Book Name " + dict.Key);
-                    Person personResult = dict.Value;
-                    Console.WriteLine("First Name : {0} , Last Name : {1} , Address : {2} , City : {3} , State : {4} , Zip : {5} , Phone Number : {6} ",
-                        personResult.FirstName, personResult.LastName, personResult.Address, personResult.City, personResult.State, personResult.Zip, personResult.PhoneNumber);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Please enter person name");
             }
         }
     }
