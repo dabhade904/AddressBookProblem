ff6df61fd115b6e80d21f48d79ee523e4f102716
dabhade904 <dabhade904@gmail.com>
2022-09-07 12:53:53 +0530

    Added : text file to perfrom operations

---
+++ AddressBook/FileIoReadWrite.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public static class FileIoReadWrite
+    {
+        public static string filePath = "C:\\Users\\kamlesh\\Desktop\\dotNet\\AddressBookProblem\\AddressBook\\data.txt";
+        public static List<String> lines = new List<String>();
+        public static void ReadData()
+        {
+            if (File.Exists(filePath))
+            {
+                lines = File.ReadAllLines(filePath).ToList();
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+        }
+        public static void WriteData()
+        {
+            lines.Add("mahi, gaikwad, nandgaon, nandgoan, mh, harsh@gmail.com, 431133");
+            File.WriteAllLines(filePath, lines);
+            Console.ReadLine();
+        }
+        
+        public static void FileDelete()
+        {
+            if (File.Exists(filePath))
+            {
+                 File.Delete(filePath);
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+        }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -4,42 +4,68 @@
     {
         public static void Main(string[] args)
         {
-            AddressBook obj = new AddressBook();
-            while (true)
+            Console.WriteLine("Enter your choice");
+            int userInpute = Convert.ToInt32(Console.ReadLine());
+            if (userInpute == 1)
             {
-                Console.WriteLine("Enter your choice: \n 1. create new person \n 2. Display Data \n 3. Edit person Data \n 4. Delete Person Data \n 5. Search Person \n 6. Enter city or state to search person \n 7.Enter city or state to search multiple person \n 0 .Exit");
-                int input = Convert.ToInt32(Console.ReadLine());
-                switch (input)
+                AddressBook obj = new AddressBook();
+                while (true)
                 {
-                    case 1:
-                        obj.Createuser();
-                        break;
-                    case 2:
-                        obj.Display();
-                        break;
-                    case 3:
-                        obj.EditPersonInfo();
-                        break;
-                    case 4:
-                        obj.DeletePerson();
-                        break;
-                    case 5:
-                        obj.SearchPerson();
-                        break;
-                    case 6:
-                        obj.SearchPersonBasedOnCityAndState();
-                        break;
-                    case 7:
-                        obj.GetNumberOfPerson();
-                        break;
-                    case 8:
-                        obj.SortingByPersonName();
-                        break;
-                    case 9:
-                        obj.SortingByPersonCityStateOrZip();
-                        break;
+                    Console.WriteLine("Enter your choice: \n 1. create new person \n 2. Display Data \n 3. Edit person Data \n 4. Delete Person Data \n 5. Search Person \n 6. Enter city or state to search person \n 7.Enter city or state to search multiple person \n 0 .Exit");
+                    int input = Convert.ToInt32(Console.ReadLine());
+                    switch (input)
+                    {
+                        case 1:
+                            obj.Createuser();
+                            break;
+                        case 2:
+                            obj.Display();
+                            break;
+                        case 3:
+                            obj.EditPersonInfo();
+                            break;
+                        case 4:
+                            obj.DeletePerson();
+                            break;
+                        case 5:
+                            obj.SearchPerson();
+                            break;
+                        case 6:
+                            obj.SearchPersonBasedOnCityAndState();
+                            break;
+                        case 7:
+                            obj.GetNumberOfPerson();
+                            break;
+                        case 8:
+                            obj.SortingByPersonName();
+                            break;
+                        case 9:
+                            obj.SortingByPersonCityStateOrZip();
+                            break;
+                        case 0:
+                            Environment.Exit(0);
+                            break;
+                    }
+                }
+            }
+            else if (userInpute == 2)
+            {
+                Console.WriteLine("Enter your choice: \n 1. Read Data \n 2. Write Data \n 3 Delete File \n 0 .Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
                     case 0:
                         Environment.Exit(0);
+                        break;
+                    case 1:
+                        FileIoReadWrite.ReadData();
+                        break;
+                        break;
+                    case 2:
+                        FileIoReadWrite.WriteData();
+                        break;
+                    case 3:
+                        FileIoReadWrite.FileDelete();
                         break;
                 }
             }
